c36a1d3cd9a9fa4226995d74a46205b3d76c6722
David Whitehurst <dlwhitehurst@gmail.com>
2020-12-02 16:08:32 -0500

    Initial Commit

---
+++ istatus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  istatus
+//
+//  Created by David Whitehurst on 12/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ istatus/istatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  istatusApp.swift
+//  istatus
+//
+//  Created by David Whitehurst on 12/2/20.
+//
+
+import SwiftUI
+
+@main
+struct istatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
